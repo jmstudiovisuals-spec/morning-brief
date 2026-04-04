9bcce6569777cc2ec3ad2dea03deb2a02821ee45
jmstudiovisuals-spec <jmstudiovisuals@gmail.com>
2026-04-04 03:01:11 +0200

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,20 @@
+
+Copier
+
+const CACHE_NAME = 'jmstudio-brief-v1';
+const urlsToCache = ['/'];
+ 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+ 
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
+  );
+});
+ 
